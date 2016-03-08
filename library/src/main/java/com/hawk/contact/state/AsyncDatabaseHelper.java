7d3d87ce5fb4b23f43347d70d9969bf2720b9329
Hawk <leqinde@163.com>
2016-03-08 17:34:34 +0800

    Library init 2

---
+++ library/src/main/java/com/hawk/contact/state/AsyncDatabaseHelper.java
@@ -0,0 +1,20 @@
+package com.hawk.contact.state;
+
+import com.hawk.contact.model.ContactUserProfile;
+
+/**
+ * Created by Administrator on 2016/3/8.
+ */
+public interface AsyncDatabaseHelper {
+
+    public void getUserProfile(String username, Callback<ContactUserProfile> callback);
+
+    public void putUserProfile(ContactUserProfile contactUserProfile);
+
+    public void deleteUserProfile(ContactUserProfile contactUserProfile);
+
+    public interface Callback<T> {
+        public void onFinished(T result);
+    }
+
+}
---
+++ library/src/main/java/com/hawk/contact/state/AsyncDatabaseHelperImpl.java
@@ -0,0 +1,24 @@
+package com.hawk.contact.state;
+
+import com.hawk.contact.model.ContactUserProfile;
+
+/**
+ * Created by Administrator on 2016/3/8.
+ */
+public class AsyncDatabaseHelperImpl implements AsyncDatabaseHelper {
+
+    @Override
+    public void getUserProfile(String username, Callback<ContactUserProfile> callback) {
+
+    }
+
+    @Override
+    public void putUserProfile(ContactUserProfile contactUserProfile) {
+
+    }
+
+    @Override
+    public void deleteUserProfile(ContactUserProfile contactUserProfile) {
+
+    }
+}
